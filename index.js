2d1c44fd77ac000ac49c656f6cf61e8cc7ac23a3
mohammad alameri <onex911@hotmail.com>
2022-05-24 09:09:59 +0200

    returns the file data

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var fileUp = require("express-fileupload");
 var app = express();
 
+app.use(fileUp());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse",(req,res)=>{
+res.json({name:req.files.upfile.name,type:req.files.upfile.mimetype,size:req.files.upfile.size})
+})
 
 
 
